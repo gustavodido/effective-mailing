477ead6d4e306af25042f83eb1858a759cc5107b
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-02-01 21:25:40 -0800

    Add record insertion.

--- EffectiveMailing/EffectiveMailing/Program.cs
+++ EffectiveMailing/EffectiveMailing/Program.cs
@@ -34,4 +34,17 @@ namespace EffectiveMailing
             }               
        }
     }
+
+     static void InserRecord(OracleConnection conn, string arquivo, int acao, string operadora, string atributo, string valor)
+        {
+                OracleCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO " + Properties.Settings.Default.TableName + " (DT_CARGA, ARQUIVO, ACAO, OPERADORA, ATRIBUTO, VALOR) VALUES (sysdate, :1, :2, :3, :4, :5)";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new OracleParameter(":1", OracleDbType.Varchar2)).Value = arquivo;
+                cmd.Parameters.Add(new OracleParameter(":2", OracleDbType.Varchar2)).Value = acao;
+                cmd.Parameters.Add(new OracleParameter(":3", OracleDbType.Varchar2)).Value = operadora;
+                cmd.Parameters.Add(new OracleParameter(":4", OracleDbType.Varchar2)).Value = atributo;
+                cmd.Parameters.Add(new OracleParameter(":5", OracleDbType.Varchar2)).Value = valor;
+                cmd.ExecuteNonQuery();
+        }
 }
