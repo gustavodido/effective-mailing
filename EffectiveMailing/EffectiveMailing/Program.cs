7aa65e515cba384653ede0b5acbb54a0f747f8d7
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-02-01 21:22:49 -0800

    Add project structure.

---
+++ EffectiveMailing/EffectiveMailing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Data;
+using Oracle.DataAccess.Client;
+
+using Microsoft.Office.Interop.Excel;
+
+namespace EffectiveMailing
+{
+    class Program
+    {
+    }
+}
