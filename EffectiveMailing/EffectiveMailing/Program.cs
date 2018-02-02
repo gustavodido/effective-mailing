2ca2e3e8717cf11d2f4daef518d10d982276a8a4
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-02-01 21:26:28 -0800

    Open file and start processing.

--- EffectiveMailing/EffectiveMailing/Program.cs
+++ EffectiveMailing/EffectiveMailing/Program.cs
@@ -1,43 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Data;
-using Oracle.DataAccess.Client;
+﻿        static StreamWriter fs;
 
-using Microsoft.Office.Interop.Excel;
-
-namespace EffectiveMailing
-{
-     static StreamWriter fs;
-     
-    class Program
-    {
-    }
-
-    static void CreateTable()
-    {
-        using (OracleConnection conn = new OracleConnection(Properties.Settings.Default.ConnectionString))
+        static void Main(string[] args)
         {
-            conn.Open();
-            OracleCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "CREATE TABLE " + Properties.Settings.Default.TableName + " (DT_CARGA DATE, ARQUIVO VARCHAR2(255), ACAO NUMBER, OPERADORA VARCHAR2(255), ATRIBUTO VARCHAR2(100), VALOR VARCHAR(255)) NOLOGGING";
-            cmd.CommandType = CommandType.Text;
             try
             {
-                cmd.ExecuteNonQuery();
+                fs = new StreamWriter(Properties.Settings.Default.Log, true);
+
+                int currAction = 1;
+                Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+                CreateTable();
+                WriteLog("Criando tabela " + Properties.Settings.Default.TableName);
+
+                WriteLog("Criando conexao com banco");
+                OracleConnection conn = new OracleConnection(Properties.Settings.Default.ConnectionString);
+                conn.Open();
+
+                WriteLog("Carregando planilhas XLSX");
+                foreach (string filename in Directory.GetFiles(Properties.Settings.Default.Directory, "*.xlsx"))
+                {
+                }
+            
+                xlApp.Quit();
             }
-            catch(OracleException ex)
+            catch (Exception ex)
             {
                 WriteLog(ex.Message);
-                if (ex.Number != 955)
-                    throw ex;
-            }               
-       }
-    }
+            }
 
-     static void InserRecord(OracleConnection conn, string arquivo, int acao, string operadora, string atributo, string valor)
+            fs.Close();
+        }
+
+        static void WriteLog(string log)
+        {
+            fs.WriteLine(String.Format("[{0}] -> {1}", DateTime.Now.ToString("dd/MM/yyyy hh:mm"), log));
+        }
+
+        static void CreateTable()
+        {
+            using (OracleConnection conn = new OracleConnection(Properties.Settings.Default.ConnectionString))
+            {
+                conn.Open();
+                OracleCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "CREATE TABLE " + Properties.Settings.Default.TableName + " (DT_CARGA DATE, ARQUIVO VARCHAR2(255), ACAO NUMBER, OPERADORA VARCHAR2(255), ATRIBUTO VARCHAR2(100), VALOR VARCHAR(255)) NOLOGGING";
+                cmd.CommandType = CommandType.Text;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch(OracleException ex)
+                {
+                    WriteLog(ex.Message);
+                    if (ex.Number != 955)
+                        throw ex;
+                }               
+           }
+        }
+
+        static void InserRecord(OracleConnection conn, string arquivo, int acao, string operadora, string atributo, string valor)
         {
                 OracleCommand cmd = conn.CreateCommand();
                 cmd.CommandText = "INSERT INTO " + Properties.Settings.Default.TableName + " (DT_CARGA, ARQUIVO, ACAO, OPERADORA, ATRIBUTO, VALOR) VALUES (sysdate, :1, :2, :3, :4, :5)";
@@ -49,10 +69,3 @@ namespace EffectiveMailing
                 cmd.Parameters.Add(new OracleParameter(":5", OracleDbType.Varchar2)).Value = valor;
                 cmd.ExecuteNonQuery();
         }
-
-             static void WriteLog(string log)
-        {
-            fs.WriteLine(String.Format("[{0}] -> {1}", DateTime.Now.ToString("dd/MM/yyyy hh:mm"), log));
-        }
-
-}
